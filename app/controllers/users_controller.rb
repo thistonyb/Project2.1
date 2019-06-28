44fd14bc0ca3f72942f98c9a978b4f073576f326
TonyB <this.tonybaker@gmail.com>
2019-06-28 11:23:26 -0700

    Edit signup get and post, create rocks erb, edit welcome

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     get '/signup' do
       if Helpers.is_signed_in?(session)
-        redirect to '/rocks'
+        redirect to '/welcome'
       else
         erb :'/users/create_user'
       end
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
         if !(params.has_value(""))
             user = User.create(params)
             session["user_id"] = user.id
-            redirect to '/rocks'
+            redirect to '/welcome'
         else
             redirect to '/signup'
         end
