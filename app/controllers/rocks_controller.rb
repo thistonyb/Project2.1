9996d8cf6a6b660b882e30db08b679a2cc2f1600
TonyB <this.tonybaker@gmail.com>
2019-06-28 13:57:42 -0700

    Edit get rocks controller, create get rocks/new controller
    Also, edit rocks erb to all_rocks erb

--- app/controllers/rocks_controller.rb
+++ app/controllers/rocks_controller.rb
@@ -1,12 +1,24 @@
 class RocksController < ApplicationController
-    get '/rocks' do
+    
+    get '/rocks/all' do
         if Helper.is_signed_in(session)
             @rocks = Rock.all
-            erb :'/rocks/rocks'
+            erb :'/rocks/all_rocks'
         else
             redirect to '/users/signin'
         end
     end
+
+    get '/rocks/new' do
+        user = Helpers.current_user(session)
+        if user.nil?
+            redirect to '/signin'
+        else
+            erb :'/rocks/create_rock'
+        end
+    end
+
+
     
 
 
